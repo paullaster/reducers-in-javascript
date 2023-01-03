5175ba469163de106a1dcc4753dc938480fc0ab7
paullaster <paullasterokoth98@gmail.com>
2023-01-03 09:59:38 +0300

    append to files

--- reducers.js
+++ reducers.js
@@ -24,7 +24,7 @@ const toDoReducer = (state, action) => {
       state.map(item => {
         if (item.id === action.payload.id) {
           return {
-            ...state,
+            ...item,
             status: true,
           };
         }
