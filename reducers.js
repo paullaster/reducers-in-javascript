ef3a87ec99ac11a2158f0a3b636877441b1dba9c
paullaster <paullasterokoth98@gmail.com>
2023-01-03 09:33:44 +0300

    append to files

--- reducers.js
+++ reducers.js
@@ -1,47 +1,48 @@
-const colorState = (color, action) =>{
-    switch (action.type) {
-        case 'danger':
-            return 'red';
-            break;
-        case 'success':
-            return 'green';
-            break;
-        default:
-            return 'blue';
-            break;
-    }
+const colorState = (color, action) => {
+  switch (action.type) {
+    case "danger":
+      return "red";
+      break;
+    case "success":
+      return "green";
+      break;
+    default:
+      return "blue";
+      break;
+  }
 };
 
-const color = 'green';
+const color = "green";
 const tinkerColor = {
-    type: 'success',
-}
+  type: "success"
+};
 //console.log (colorState (color, tinkerColor))
 
-const toDoReducer =  (state, action) => {
-    switch(action.type){
-        case 'DONE':
-           state.map ( (item) => {
+const toDoReducer = (state, action) => {
+  switch (action.type) {
+    case "DONE":
+      state.map(item => {
+        if(
             item.id === action.payload.id
-                
-           })
-    }
+        )
+      });
+  }
 };
 
 const todo = [
-    {
-        activity: 'Read react',
-        status: false,
-        id: 1,
-    },
-    {
-        activity: 'Practise react code',
-        status: false,
-        id: 2,
-    },
-    {
-        activity: 'Read JavaScript reducer',
-        status: false,
-        id: 3,
-    }
+  {
+    activity: "Read react",
+    status: false,
+    id: 1
+  },
+  {
+    activity: "Practise react code",
+    status: false,
+    id: 2
+  },
+  {
+    activity: "Read JavaScript reducer",
+    status: false,
+    id: 3
+  }
 ];
